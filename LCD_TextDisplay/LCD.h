5fe9ce280d85b6148a0c33e580277a2be9c4bb87
gjc13 <amtcmp@126.com>
2015-05-28 21:51:13 +0800

    create

---
+++ LCD_TextDisplay/LCD.h
@@ -0,0 +1,28 @@
+#include "MBED.h"
+
+
+
+
+
+
+class LCD {
+public:					//Strobe			//CLK					//Data
+ 
+ LCD(PinName pin1, PinName pin2, PinName pin3, PinName pin4, PinName pin5, PinName pin6, PinName pin7, PinName pin8, PinName pin9, PinName pin10); 
+		void ClearDisplay();
+		void Print_String(char* Pbuffer,char line_num=1, char offset=0);
+
+
+
+private:
+
+	BusInOut _DataBus;
+  DigitalOut 		_RS;
+	DigitalOut 		_Enable;
+
+		void Initialize();
+		void WriteLCDData(char c, char Pos);
+		void WriteLCDCmd(char c);
+
+
+};
